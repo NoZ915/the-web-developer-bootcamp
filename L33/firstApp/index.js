91f284e3512cb1584a467e7846182e64a041d06e
YuXuan Lin <tina0915tw0915@gmai.com>
2022-02-18 10:44:01 +0800

    finished: L334 request & response object

--- L33/firstApp/index.js
+++ L33/firstApp/index.js
@@ -2,8 +2,9 @@ const express = require("express")
 const app = express()
 const port = 3000
 
-app.use(()=>{
+app.use((req,res)=>{
     console.log("we got a new request!")
+    res.send("we got your request! this is a response!")
 })
 
 app.listen(port, ()=>{
