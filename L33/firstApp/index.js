4ee12ab3352a0e32d930f1a6fb5e4d7f57cfcf4c
YuXuan Lin <tina0915tw0915@gmai.com>
2022-02-18 12:26:31 +0800

    finished: L336 express path parameters

--- L33/firstApp/index.js
+++ L33/firstApp/index.js
@@ -2,9 +2,26 @@ const express = require("express")
 const app = express()
 const port = 3000
 
-app.use((req,res)=>{
-    console.log("we got a new request!")
-    res.send("we got your request! this is a response!")
+// app.use((req,res)=>{
+//     console.log("we got a new request!")
+//     res.send("we got your request! this is a response!")
+// })
+
+app.get("/r/:subreddit/:postID",(req,res)=>{
+    const {subreddit, postID} = req.params
+    res.send(`<h1>vewing the postID: ${postID} on the ${subreddit} subreddit</h1>`)
+})
+
+app.get("/",(req,res)=>{
+    res.send("this is a home page!")
+})
+
+app.get("/cats", (req,res)=>{
+    res.send("meow!")
+})
+
+app.get("/dogs",(req,res)=>{
+    res.send("woof!")
 })
 
 app.listen(port, ()=>{
